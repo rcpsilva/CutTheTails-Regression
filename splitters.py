dd5852a8e1c8ba510155fed35c16855a0838971c
rcpsilva <rcpsilva@gmail.com>
2022-11-06 02:11:56 -0300

    [feat] split data by quantile

---
+++ splitters.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def split_by_quantile(df,target,q):
+    '''
+    Args
+
+        df: original dataset
+        target: target variable
+        q: list of upper and lower quantile
+
+    Returns:
+
+        lbdf: data below the lower quantile
+        nddf: data between quantiles
+        ubdf: date over the upper quantile
+
+    '''
+
+    q = np.quantile(df[[target]],q = q)
+
+    lbdf = df[df[target] <= q[0]]
+    ubdf = df[df[target] >= q[1]]
+    nddf = df[(df[target] > q[0]) & (df[target] < q[1])]
+
+    return lbdf,nddf,ubdf
