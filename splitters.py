7934f2709909892cac014ee7e0f9c59a10c4c93d
rcpsilva <rcpsilva@gmail.com>
2022-11-06 12:42:47 -0300

    [chore] Implemented the ML pipeline

--- splitters.py
+++ splitters.py
@@ -1,7 +1,29 @@
 import numpy as np
+from copy import deepcopy
+
+def fit_tail_classifier(X,y_tail,model):
+    model.fit(X,y_tail)
+    return model
+
+def fit_tail_models(X,y,y_tail,model):
+    models = [deepcopy(model) for i in [0,1,2]]
+    for i in [0,1,2]:
+        models[i].fit(X[y_tail==i],y[y_tail==i]) 
+    return models
+
+def tail_predict(x,tail_classifier,tail_models):
+    tail_class = tail_classifier.predict([x])
+    return tail_models[tail_class[0]].predict([x])
+
+def batch_tail_predict(X,tail_classifier,tail_models):
+    preds = []
+    for x in X: #very inneficient
+        preds.append(tail_predict(x,tail_classifier,tail_models))
+    return np.array([p[0] for p in preds])
 
 def split_by_quantile_class(df,target,q):
-    '''
+    ''' Creates a column called 'quant_class' that identifies the samples in the 
+        target distribution tails. Lower tail:0, normal:1, upper tail:2
     Args
 
         df: original dataset
@@ -10,9 +32,7 @@ def split_by_quantile_class(df,target,q):
 
     Returns:
 
-        lbdf: data below the lower quantile
-        nddf: data between quantiles
-        ubdf: date over the upper quantile
+        df: updated dataframe
 
     '''
 
@@ -22,13 +42,13 @@ def split_by_quantile_class(df,target,q):
 
     for index, row in df.iterrows():
         if row[target] <= q[0]:
-            quant_class.append(1)
+            quant_class.append(0)
         elif (row[target] > q[0]) and (row[target] < q[1]):
-            quant_class.append(2)
+            quant_class.append(1)
         elif row[target] >= q[1]:
-            quant_class.append(3)
+            quant_class.append(2)
     
-    df['quant_class'] = quant_class
+    df['tail_class'] = quant_class
 
     return df
 
