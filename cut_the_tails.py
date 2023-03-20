1e319ee34a40dd922af8c99eec2f755456892c67
rcpsilva <rcpsilva@gmail.com>
2023-03-20 19:37:20 -0300

    Functions fit and predict adhere to the google style guide

--- cut_the_tails.py
+++ cut_the_tails.py
@@ -37,37 +37,78 @@ def predict_cut_the_tail_proxy(X,quantiles,IQR,proxy_model,lower_tail_model,norm
 
 
 def fit_cut_the_tail(X,y,quantiles,tail_classifier,lower_tail_model,normal_model,upper_tail_model):
+    """This function takes in the features X and the target variable y, 
+    as well as some parameters to specify the quantiles for defining the tails of the distribution
+    and the models to fit for each tail.
+
+    Args:
+        x: A numpy array of features.
+        y: A numpy array of the target variable.
+        quantiles: A list of two values specifying the quantiles to use for defining the tails of the distribution.
+        tail_classifier: A machine learning model to fit for assigning observations to each tail class.
+        lower_tail_model: A machine learning model to fit for the lower tail.
+        normal_model: A machine learning model to fit for the normal range.
+        upper_tail_model: A machine learning model to fit for the upper tail.
+    
+    Returns:
+        None
+    
+    """
+
+
+    # Calculate the quantiles of the target variable y to define the lower and upper tails
     q = np.quantile(y,q = quantiles)
 
+    # Convert the target variable y to a numpy array
     y = np.array(y)
 
-    # get tail classes
+    # Define tail classes for each observation based on the calculated quantiles
+    # Assign 1 for lower tail, 2 for normal, and 3 for upper tail
     y_tail = (y <= q[0])*1 + (y >= q[1])*3 + ((y > q[0]) & (y < q[1]))*2
 
-    # fit tail classifier
+    # Fit the tail classifier using the features X and the assigned tail classes
     tail_classifier.fit(X,y_tail)
 
-    # fit lower tail model
+    # Fit the lower tail model using the subset of X and y where y_tail == 1
     lower_tail_model.fit(X[y_tail==1],y[y_tail==1])
 
-    # fit normal model
+    # Fit the normal model using the subset of X and y where y_tail == 2
     normal_model.fit(X[y_tail==2],y[y_tail==2])
 
-    # fit upper tail model
+    # Fit the upper tail model using the subset of X and y where y_tail == 3
     upper_tail_model.fit(X[y_tail==3],y[y_tail==3])
-    
 
-def predict_cut_the_tails(X,tail_classifier,lower_tail_model,normal_model,upper_tail_model):
 
-    y_tail = tail_classifier.predict(X)
 
-    y_lower = lower_tail_model.predict(X)
-    y_normal = normal_model.predict(X)
-    y_upper = upper_tail_model.predict(X)
+def predict_cut_the_tails(x, tail_classifier, lower_tail_model, normal_model, upper_tail_model):
+    """
+    Predicts the target variable based on a set of features x, using different models for the lower, normal, and upper
+    tails of the distribution based on a tail classifier.
 
-    y = y_lower*(y_tail==1) + y_normal*(y_tail==2) + y_upper*(y_tail==3)
+    Args:
+        x: A numpy array of features.
+        tail_classifier: A trained machine learning model to classify observations into tail classes.
+        lower_tail_model: A trained machine learning model for the lower tail.
+        normal_model: A trained machine learning model for the normal range.
+        upper_tail_model: A trained machine learning model for the upper tail.
 
-    return y  
+    Returns:
+        A numpy array of predicted values for the target variable.
+    """
+
+    # Use the tail classifier to predict the tail class for each observation in x
+    y_tail = tail_classifier.predict(x)
+
+    # Use the corresponding model to predict the target variable for each observation in x,
+    # based on its assigned tail class
+    y_lower = lower_tail_model.predict(x[y_tail == 1])
+    y_normal = normal_model.predict(x[y_tail == 2])
+    y_upper = upper_tail_model.predict(x[y_tail == 3])
+
+    # Combine the predicted values for each observation based on its assigned tail class
+    y = y_lower * (y_tail == 1) + y_normal * (y_tail == 2) + y_upper * (y_tail == 3)
+
+    return y
 
 
 def fit_tail_classifier(X,y_tail,model):
